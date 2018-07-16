5bdcbbd370928a961debba26aa2dc01840c9d47a
danilamedjo <medjo.danila@gmail.com>
2018-07-16 09:56:06 +0200

    set up project environment #1

---
+++ Survey/src/main/java/rs/levi9/survey/SurveyApplication.java
@@ -0,0 +1,12 @@
+package rs.levi9.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyApplication.class, args);
+	}
+}
