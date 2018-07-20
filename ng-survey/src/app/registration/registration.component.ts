73ff868f16ee669795f4c1cb82ae9fcc06ac209f
TamaraBilinac <bilinac.tamara@gmail.com>
2018-07-20 13:28:14 +0200

    fixed modals

--- ng-survey/src/app/registration/registration.component.ts
+++ ng-survey/src/app/registration/registration.component.ts
@@ -15,7 +15,6 @@ export class RegistrationComponent implements OnInit {
   public user: SurveyUser;
   public errorMessage : string;
   public pass: string;
-  public submitted: boolean;
   public message: string;
 
   constructor(private registrationService: RegistrationService, private router: Router) { }
@@ -25,25 +24,16 @@ export class RegistrationComponent implements OnInit {
   }
 
   public takeSignUpData(form: NgForm) {
- 
-    console.log(form.value.username);
-    console.log(form.value.email);
-    console.log(form.value.password);
-
-    this.insertNewUser(form.value.username, form.value.email, form.value.password);
-      this.submitted = false;
+      this.insertNewUser(form.value.username, form.value.email, form.value.password);
       this.registrationService.post(this.user).subscribe(data => {
       this.user = data;     
-      this.submitted = true;
     },
     (error) => { 
       this.errorMessage = error;
-      this.submitted = false;
       this.message = "Couldn't register, user with that username or e-mail already exists!";
     },
     () => {
       console.log("User registered!");
-      this.submitted = true;
       this.message = "You registered succcessfully!";
       form.reset();
     });
